cfdbcc6b8c66573d539505a40c17bc38a906378a
rizki-rianto666 <115854620+rizki-rianto666@users.noreply.github.com>
2022-12-24 11:04:09 +0700

    Menambah pertemuan 12

---
+++ FungsiStatic.java
@@ -0,0 +1,18 @@
+public class FungsiStatic {
+	static void minum(String minuman){ //fungsi static
+		System.out.println("Saya suka minum " + minuman);
+	}
+	// Fungsi non-static
+	void makan(String makanan){
+		System.out.println("Saya suka makan " + makanan);
+	}
+	// fungsi main
+	public static void main(String[] args) {
+		// pemanggilan fungsi static
+		minum("Kopi");
+		// mambuat instansiasi objek saya dari class FungsiStatic
+		FungsiStatic saya = new FungsiStatic();
+		// pemanggilan fungsi non-static
+		saya.makan("Pisang Goreng");
+	}
+}
---
+++ NamaClass.java
@@ -0,0 +1,13 @@
+public class NamaClass {
+	String variabel1;
+	String variabel2;
+	void namaMethod(){//ISI METHOD 
+	}
+	
+	public static void main(String[]argg) {
+		NamaClass namaObjek = new NamaClass();
+		System.out.println(namaObjek.variabel1);
+		System.out.println(namaObjek.variabel2);
+		namaObjek.namaMethod();
+	}
+}
